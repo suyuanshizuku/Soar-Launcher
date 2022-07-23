deed11b4f2c2a0d2a106c3c98b8f873363eac05b
EldoDebug <75922660+EldoDebug@users.noreply.github.com>
2022-07-23 23:08:36 +0900

    Update Soar.java

--- soar/Soar.java
+++ soar/Soar.java
@@ -46,7 +46,7 @@ public class Soar extends Base{
 	
 	private ArrayList<String> logs = new ArrayList<String>();
 	
-	private String version = "2.2.2";
+	private String version = "2.2.3";
 
 	private String username, token, refreshToken, id;
 	
